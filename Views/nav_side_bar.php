6a40b3371c01a04e229f423046d4020bed89fd3f
Mc Allain Casindad <AizenTestingServer@gmail.com>
2022-07-07 09:48:33 +0800

    Optimizations

--- Views/nav_side_bar.php
+++ Views/nav_side_bar.php
@@ -9,109 +9,123 @@
                     onerror="this.src='../Assets/img/profile_imgs/no_image_found.jpeg';">
                 <span class="fw-bold">WSAP IP</span>
             </div>
-            <ul class="side-nav-list px-1 mt-4 text-center">
-                <li>
-                    <a <?php
-                    if ($target_active == "dashboard") { ?>
-                        class="active" <?php
-                    } else { ?>
-                        class="inactive"
-                        href="dashboard.php" <?php
-                    } ?>>
-                        <div class="icon-container">
-                            <i class="fa-solid fa-table-columns fa-2x"></i>
-                        </div>
-                        Dashboard
-                    </a>
-                </li>
-                <li class="dropdown-toggler">
-                    <a <?php
-                    if ($target_active == "brands") { ?>
-                        class="active" <?php
-                    } else { ?>
-                        class="inactive"
-                        href="brands.php" <?php
-                    } ?>>
-                        <div class="icon-container">
-                            <i class="fa-solid fa-bullhorn fa-2x"></i>
-                        </div> Brands
-                    </a>
-                    <ul class="dropdown-list">
-                        <!--
-                        <li><a href="#">Organizational Chart</a></li>
-                        <li><a href="#">Promotions</a></li>
-                        <li><a href="brands.php">Websites</a></li>
-                        -->
-                    </ul>
-                </li>
-                <li class="dropdown-toggler">
-                    <a <?php
-                    if ($target_active == "attendance") { ?>
-                        class="active" <?php
-                    } else { ?>
-                        class="inactive" <?php
-                    } ?>>
-                        <div class="icon-container">
-                            <i class="fa-solid fa-clock fa-2x"></i>
-                        </div> Attendance
-                    </a>
-                    <ul class="dropdown-list text-start">
-                        <li><a href="attendance.php">My Attendance</a></li>
-                        <li><a href="interns_attendance.php">Interns' Attendance</a></li>
-                        <li><a href="calendar.php?month=<?= $date->getMonthName() ?>&year=<?= $date->getYear() ?>">Calendar</a></li>
-                        <li><a href="daily_time_record.php">Interns' DTR</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown-toggler">
-                    <a <?php
-                    if ($target_active == "tasks") { ?>
-                        class="active" <?php
-                    } else { ?>
-                        class="inactive" <?php
-                    } ?>>
-                        <div class="icon-container">
-                            <i class="fa-solid fa-tasks fa-2x"></i>
-                        </div> Tasks
-                    </a>
-                    <ul class="dropdown-list text-start">
-                        <li><a href="tasks.php">My Tasks</a></li>
-                        <li><a href="interns_tasks.php">Interns' Tasks</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown-toggler">
-                    <a <?php
-                    if ($target_active == "interns") { ?>
-                        class="active" <?php
-                    } else { ?>
-                        class="inactive" <?php
-                    } ?>>
-                        <div class="icon-container">
-                            <i class="fa-solid fa-users fa-2x"></i>
-                        </div> Interns
-                    </a>
-                    <ul class="dropdown-list text-start">
-                        <li><a href="profile.php">My Profile</a></li>
-                        <li><a href="interns.php">Interns</a></li>
-                        <li><a href="admins.php">Admins</a></li>
-                        <li><a href="roles.php">Roles</a></li>
-                        <li><a href="assign_roles.php">Assign Roles</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a <?php
-                    if ($target_active == "auditLogs") { ?>
-                        class="active" <?php
-                    } else { ?>
-                        class="inactive"
-                        href="audit_logs.php?day=<?= $date->getDay() ?>&month=<?= $date->getMonthName() ?>&year=<?= $date->getYear() ?>" <?php
-                    } ?>>
-                        <div class="icon-container">
-                            <i class="fa-solid fa-book fa-2x"></i>
-                        </div>
-                        Audit Logs
-                    </a>
-                </li>
-            </ul>
+            <div class="navs py-4 d-flex justify-content-center align-items-center">
+                <ul class="side-nav-list p-0 text-center">
+                    <li>
+                        <a <?php
+                        if ($target_active == "dashboard") { ?>
+                            class="active" <?php
+                        } else { ?>
+                            class="inactive"
+                            href="dashboard.php" <?php
+                        } ?>>
+                            <div class="icon-container">
+                                <i class="fa-solid fa-table-columns fa-2x"></i>
+                            </div>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="dropdown-toggler">
+                        <a <?php
+                        if ($target_active == "brands") { ?>
+                            class="active" <?php
+                        } else { ?>
+                            class="inactive"
+                            href="brands.php" <?php
+                        } ?>>
+                            <div class="icon-container">
+                                <i class="fa-solid fa-bullhorn fa-2x"></i>
+                            </div> Brands
+                        </a>
+                        <ul class="dropdown-list">
+                            <!--
+                            <li><a href="#">Organizational Chart</a></li>
+                            <li><a href="#">Promotions</a></li>
+                            <li><a href="brands.php">Websites</a></li>
+                            -->
+                        </ul>
+                    </li>
+                    <li class="dropdown-toggler">
+                        <a <?php
+                        if ($target_active == "attendance") { ?>
+                            class="active" <?php
+                        } else { ?>
+                            class="inactive" <?php
+                        } ?>>
+                            <div class="icon-container">
+                                <i class="fa-solid fa-clock fa-2x"></i>
+                            </div> Attendance
+                        </a>
+                        <ul class="dropdown-list text-start">
+                            <li><a href="attendance.php">My Attendance</a></li>
+                            <li><a href="interns_attendance.php">Interns' Attendance</a></li>
+                            <li><a href="calendar.php?month=<?= $date->getMonthName() ?>&year=<?= $date->getYear() ?>">Calendar</a></li>
+                            <li><a href="daily_time_record.php">Interns' DTR</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown-toggler">
+                        <a <?php
+                        if ($target_active == "tasks") { ?>
+                            class="active" <?php
+                        } else { ?>
+                            class="inactive" <?php
+                        } ?>>
+                            <div class="icon-container">
+                                <i class="fa-solid fa-tasks fa-2x"></i>
+                            </div> Tasks
+                        </a>
+                        <ul class="dropdown-list text-start">
+                            <li><a href="tasks.php">My Tasks</a></li>
+                            <li><a href="interns_tasks.php">Interns' Tasks</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown-toggler">
+                        <a <?php
+                        if ($target_active == "interns") { ?>
+                            class="active" <?php
+                        } else { ?>
+                            class="inactive" <?php
+                        } ?>>
+                            <div class="icon-container">
+                                <i class="fa-solid fa-users fa-2x"></i>
+                            </div> Interns
+                        </a>
+                        <ul class="dropdown-list text-start">
+                            <li><a href="profile.php">My Profile</a></li>
+                            <li><a href="interns.php">Interns</a></li>
+                            <li><a href="admins.php">Admins</a></li>
+                            <li><a href="roles.php">Roles</a></li>
+                            <li><a href="assign_roles.php">Assign Roles</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a <?php
+                        if ($target_active == "auditLogs") { ?>
+                            class="active" <?php
+                        } else { ?>
+                            class="inactive"
+                            href="audit_logs.php?day=<?= $date->getDay() ?>&month=<?= $date->getMonthName() ?>&year=<?= $date->getYear() ?>" <?php
+                        } ?>>
+                            <div class="icon-container">
+                                <i class="fa-solid fa-book fa-2x"></i>
+                            </div>
+                            Audit Logs
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex justify-content-center align-items-center mt-3">
+                <ul class="side-nav-list p-0 text-center m-0">
+                    <li>
+                        <a class="text-danger" href="sign_out.php">
+                            <div class="icon-container">
+                                <i class="fa-solid fa-arrow-right-from-bracket fa-2x"></i>
+                            </div>
+                            Sign out
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div> <?php
     }
 
